d7ae1b1e63ed9d67025318a47511c01af71a3bc5
fratur <francescoturco2795@gmail.com>
2025-06-26 21:35:41 +0200

    first-commit-java

---
+++ src/StampaSaluto.java
@@ -0,0 +1,6 @@
+public class StampaSaluto {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Benvenuto Java!");
+        System.out.println("Come va?");
+    }
+}
